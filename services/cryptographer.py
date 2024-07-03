4968afa1be17c32cb80ac90bbe9d80be2be1f082
Harry Wixley <hwixley1@gmail.com>
2024-07-03 21:34:23 +0100

    GPT-commit: Refactored key encryption in steganographer functions.
    Refactored key encryption methods using Cryptographer.

---
+++ services/cryptographer.py
@@ -0,0 +1,14 @@
+from base64 import b64encode, b64decode
+from typing import Tuple
+
+class Cryptographer:
+
+    @staticmethod
+    def obf_key(text: str) -> str:
+        return b64encode(text.encode()).decode()
+    
+    @staticmethod
+    def deobf_key(obf_text: str) -> Tuple[int, int]:
+        deobf = b64decode(obf_text.encode()).decode()
+        vals = deobf.split("-")
+        return int(vals[0]), int(vals[1])
--- services/steganographer.py
+++ services/steganographer.py
@@ -1,9 +1,9 @@
 from models.XEncoder import XEncoder
 from models.XImage import XImage
 from models.XText import XText
+from services.cryptographer import Cryptographer
 import numpy as np
-from typing import Optional, Tuple
-from base64 import b64encode, b64decode
+from typing import Tuple
 
 class Steganographer:
 
@@ -17,22 +17,15 @@ class Steganographer:
         bitmask = XText(text).to_bits_array()
         
         encoded, offset = self.encoder.encode(data=image.pixels, bitmask=bitmask)
-        key = self.obf_key(text=f"{len(bitmask)}-{offset}")
+        key = Cryptographer.obf_key(text=f"{len(bitmask)}-{offset}")
         
         return encoded, key
 
     def decode_text_from_image(self, image_path: str, key: str) -> str:
         image = XImage(fname=image_path)
-        length, offset = self.deobf_key(obf_text=key)
+        length, offset = Cryptographer.deobf_key(obf_text=key)
 
         decoded_bitstr = self.encoder.decode(encoded=image.pixels, length=length, offset=offset)
         
         return self.encoder.bits_to_encoding(bitstr=decoded_bitstr)
 
-    def obf_key(self, text: str) -> str:
-        return b64encode(text.encode()).decode()
-    
-    def deobf_key(self, obf_text: str) -> Tuple[int, int]:
-        deobf = b64decode(obf_text.encode()).decode()
-        vals = deobf.split("-")
-        return int(vals[0]), int(vals[1])
